cdf450ab782b6e020a277918ebbacf1d21b8baf7
Breno Averaldo <brenoaveraldo@hotmail.com>
2026-09-04 14:19:20 -0300

    Correção do scroll

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,19 @@ import { NavigationProvider } from "./NavigationContext";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
 import { AppRoutes } from "./routes/routes";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 function AppContent() {
   return (
-    <div style={{ fontFamily: "'Poppins', 'Open Sans', 'Helvetica Neue', Arial, sans-serif", overflowX: "hidden" }}>
+    <div
+      style={{
+        fontFamily:
+          "'Poppins', 'Open Sans', 'Helvetica Neue', Arial, sans-serif",
+        overflowX: "hidden",
+      }}
+    >
+      <ScrollToTop />
+
       <Navbar />
       <AppRoutes />
       <Footer />
